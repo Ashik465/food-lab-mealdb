a9b19c13b24831a56eba30c49bff4deb8ec11b61
Ashik <helloashikul4@gmail.com>
2023-03-10 01:34:54 +0600

    card-done

---
+++ food-lab.js
@@ -0,0 +1,76 @@
+// load meals
+
+const loadMeals = async (search) => {
+  try {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    );
+     const data = await res.json();
+     showMeals(data.meals)
+  }
+  
+  catch (err) {
+    console.log("error:" + err);
+  }
+};
+
+
+
+// watch button on card 
+function watch(youtube){
+ 
+    window.location.href = youtube ;
+  }
+
+  
+  // show data 
+const showMeals =(meals) => {
+
+    console.log(meals)
+
+    const cardContainer = document.getElementById('card-container');   
+    
+
+    meals.forEach(meal => {
+
+      const div = document.createElement('div');
+      div.classList.add(
+        "card", "card-side", "bg-base-100", "shadow-xl" 
+
+      )
+     const {strMeal, strMealThumb,strInstructions ,strYoutube} =meal ;
+
+
+     div.innerHTML =`
+<figure><img class =""  src="${strMealThumb}" alt="Movie"/></figure>
+<div class="card-body">
+  <h2 class="card-title">${strMeal}</h2>
+  <p>${strInstructions.slice(0,100)}...</p>
+  <p class="text-[#FFC107] underline text-xl">View Details</p>
+
+
+  <div class="card-actions justify-end">
+    <button onclick="watch('${strYoutube}')" class="btn btn-primary">Watch</button>
+  </div>
+</div>
+`
+
+cardContainer.appendChild(div);
+
+
+
+
+    });
+
+
+
+
+
+
+
+}
+
+
+// default call 
+
+loadMeals('chicken')
