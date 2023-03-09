bdd94ff2f9b8a131edb2e185f6349b4fdda6f1be
Ashik <helloashikul4@gmail.com>
2023-03-10 02:50:54 +0600

    show-all

--- food-lab.js
+++ food-lab.js
@@ -6,7 +6,7 @@ const loadMeals = async (search) => {
       `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
     );
      const data = await res.json();
-     showMeals(data.meals)
+     showMeals(data.meals.slice(0,6))
   }
   
   catch (err) {
@@ -30,12 +30,13 @@ const showMeals =(meals) => {
 
     const cardContainer = document.getElementById('card-container');   
     
+    cardContainer.innerHTML='';
 
     meals.forEach(meal => {
 
       const div = document.createElement('div');
       div.classList.add(
-        "card", "card-side", "bg-base-100", "shadow-xl" 
+        "card", "card-side", "bg-base-100", "shadow-xl" ,"border"
 
       )
      const {strMeal, strMealThumb,strInstructions ,strYoutube} =meal ;
@@ -46,10 +47,10 @@ const showMeals =(meals) => {
 <div class="card-body">
   <h2 class="card-title">${strMeal}</h2>
   <p>${strInstructions.slice(0,100)}...</p>
-  <p class="text-[#FFC107] underline text-xl">View Details</p>
+  <p class="text-[#FFC107] underline text-xl mb-2">View Details</p>
 
 
-  <div class="card-actions justify-end">
+  <div class="card-actions ">
     <button onclick="watch('${strYoutube}')" class="btn btn-primary">Watch</button>
   </div>
 </div>
@@ -67,6 +68,26 @@ cardContainer.appendChild(div);
 
 
 
+
+}
+
+
+// show all btn 
+
+const showAll = async() =>{
+  try {
+   const search='chicken'
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    );
+     const data = await res.json();
+     showMeals(data.meals)
+     document.getElementById('show-all').classList.add('hidden');
+  }
+  
+  catch (err) {
+    console.log("error:" + err);
+  }
 
 }
 
